f81722cef87a0a3421d53b9a7687ce578013ab64
unknown <kudalen@0108M712325D.corp.espn.pvt>
2016-01-14 16:48:18 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name testRepoApp
+ * @description
+ * # testRepoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('testRepoApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testRepoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testRepoApp
+ */
+angular.module('testRepoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
